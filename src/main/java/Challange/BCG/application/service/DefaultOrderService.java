0dc98d24e7a8b54151aa1121501ed40d4dab944f
Nejatians <nejatian@outlook.com>
2021-10-21 16:35:05 +0200

    service improvments

--- src/main/java/Challange/BCG/application/service/DefaultOrderService.java
+++ src/main/java/Challange/BCG/application/service/DefaultOrderService.java
@@ -2,20 +2,35 @@ package Challange.BCG.application.service;
 
 import Challange.BCG.application.port.OrderService;
 import Challange.BCG.domain.Order;
+import Challange.BCG.domain.WatchRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class DefaultOrderService implements OrderService {
+    WatchRepository watchRepository;
+
+    public DefaultOrderService(WatchRepository watchRepository) {
+        this.watchRepository = watchRepository;
+    }
+
     @Override
     public Integer getTotal(List<String> requestList) {
         Map<String, Integer> orderCountPerItem = new HashMap<String, Integer>();
 
         // create an order
         Order order = new Order();
-        return 0;
+        requestList.forEach(item -> {
+            int count = Collections.frequency(requestList, item);
+            orderCountPerItem.put(item, count);
+        });
+        orderCountPerItem.forEach((id, count) -> order.addOrderLine(watchRepository.watchOf(id).orElseThrow(), count));
+
+        // add order line for each watch
+        return order.orderTotalPrice().amount();
     }
 }
--- src/test/java/Challange/BCG/infrastructure/InMemoryWatchRepositoryTest.java
+++ src/test/java/Challange/BCG/infrastructure/InMemoryWatchRepositoryTest.java
@@ -5,12 +5,14 @@ import Challange.BCG.domain.Watch;
 import Challange.BCG.domain.WatchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class InMemoryWatchRepositoryTest {
     @Autowired
     WatchRepository repository;
