ffeb8d2706f06f019ca70813b6aa19b23f3c3d1d
Nejatians <nejatian@outlook.com>
2021-10-21 15:28:03 +0200

    first init

---
+++ src/main/java/Challange/BCG/BcgApplication.java
@@ -0,0 +1,13 @@
+package Challange.BCG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcgApplication.class, args);
+	}
+
+}
