3073cffb7b14ff50027007968df121632d84e59f
Nejatians <nejatian@outlook.com>
2021-10-21 15:46:31 +0200

    presentation layer first init

--- src/main/java/Challange/BCG/presentation/CheckoutController.java
+++ src/main/java/Challange/BCG/presentation/CheckoutController.java
@@ -1,5 +1,21 @@
 package Challange.BCG.presentation;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping()
 public class CheckoutController {
 
+    @PostMapping("/checkout")
+    public ResponseEntity<?> checkout(@RequestBody @Valid List<String> req){
+        Integer total = 0;
+        return ResponseEntity.ok(total);
+    }
 }
